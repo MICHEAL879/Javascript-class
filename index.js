a78e53cf5aff8c4983f0c791bffb08e475b00193
micheal <kumapayimicheal1234@gmail.com>
2023-10-16 08:35:38 -0700

    i add javascript

---
+++ index.js
@@ -0,0 +1,51 @@
+// This is a calculate to add numbers
+let p = 1000
+let o =7000
+let total = p + o
+console.log(total)
+
+
+let x = 1000
+let y =7000
+let result = y - x 
+console.log(result)
+
+// This is calculating the area triangle 
+
+let height = 60
+let base = 40
+let output = height*base/2
+console.log(output)
+
+
+// This is calculating the circumference of a circle 
+
+let pi = 3.14
+let r = 12
+let circumference = 2 * pi * r 
+console.log(circumference)
+
+// This function add two numbers
+function addition(u,r){
+    let add = u + r
+    return add 
+}
+
+console.log(addition(600,900))
+
+// This function multiply two numbers
+function multiply(g,o){
+    let multiply = g * o
+    return multiply 
+}
+
+console.log(multiply(20,10))
+
+// This function subtract two numbers
+function subtract(k,l){
+    let subtract = k - l
+    return subtract
+}
+
+console.log(subtract(200,80))
+
